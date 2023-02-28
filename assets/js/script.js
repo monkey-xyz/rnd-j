b5b5cb61d773c99d7aadc3caa36a846c2f8cf5b9
Mia Borja <apk.borja@gmail.com>
2023-02-28 00:58:39 -0800

    updating printOption function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,27 +2,41 @@ const result_box = document.querySelector("#results");
 const pool_box = document.querySelector("#pool");
 const submit_box = document.querySelector("#submit-form");
 
+const cc = [];
+
 const formSubmit = (e) => {
   e.preventDefault();
 
   var opt = document.querySelector("#sg").value.trim();
 
   if (opt) {
-    printOption(opt);
+    cc.push(opt)
   } else {
     document.querySelector("error-message").innerHTML("You can't leave this empty!");
   }
+
+  printOption();
 }
 
-const printOption = (txt) => {
-  result_box.insertAdjacentHTML(
-    "beforebegin",
-    '<div>' + txt + '</div>' + '<button> <i class="fa-solid fa-xmark"></i> </button>'
-  )
+const printOption = () => {
+  var o_div = document.createElement("div");
+  var o_button = document.createElement("button");
+
+  for (let i = 0; i < cc.length; i++) {
+    result_box.appendChild(o_div);
+    o_div.textContent = cc[i];
+    o_div.appendChild(o_button);
+    o_button.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+  }
+}
+
+const deleteOption = () => {
+  console.log(this.getAttribute());
 }
 
 const selection = () => {
   Math.random();
 }
 
+printOption;
 submit_box.addEventListener("submit", formSubmit);
